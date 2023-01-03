af1ca11014a2dd822e759fc95be187aed21a6a93
Nuril Muslichin - PC <nurilmuslichin16@gmail.com>
2023-01-03 07:13:56 +0700

    Data Database

--- database/mopega.sql
+++ database/mopega.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 21 Des 2022 pada 00.02
+-- Waktu pembuatan: 03 Jan 2023 pada 01.13
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 7.4.33
 
@@ -31,19 +31,25 @@ CREATE TABLE `tb_gangguan` (
   `id_gangguan` int(11) NOT NULL,
   `id_pelanggan` int(11) NOT NULL,
   `tiket` varchar(10) NOT NULL,
-  `jenis` varchar(10) NOT NULL,
   `ket` text NOT NULL,
   `report_date` datetime NOT NULL,
   `booking_date` datetime NOT NULL,
   `teknisi` int(11) NOT NULL,
   `status` tinyint(2) NOT NULL,
-  `send_order_at` datetime NOT NULL,
-  `otw_at` datetime NOT NULL,
-  `ogp_at` datetime NOT NULL,
-  `closed_at` datetime NOT NULL,
-  `penyebab` text NOT NULL,
-  `perbaikan` text NOT NULL
+  `send_order_at` datetime DEFAULT NULL,
+  `otw_at` datetime DEFAULT NULL,
+  `ogp_at` datetime DEFAULT NULL,
+  `closed_at` datetime DEFAULT NULL,
+  `penyebab` text NOT NULL DEFAULT '-',
+  `perbaikan` text NOT NULL DEFAULT '-'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_gangguan`
+--
+
+INSERT INTO `tb_gangguan` (`id_gangguan`, `id_pelanggan`, `tiket`, `ket`, `report_date`, `booking_date`, `teknisi`, `status`, `send_order_at`, `otw_at`, `ogp_at`, `closed_at`, `penyebab`, `perbaikan`) VALUES
+(1, 1, 'IN12345', 'Wifi tidak bisa connect', '2023-01-03 06:51:03', '2023-01-03 09:51:04', 232335772, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '-', '-');
 
 -- --------------------------------------------------------
 
@@ -79,6 +85,15 @@ CREATE TABLE `tb_pelanggan` (
   `alamat` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data untuk tabel `tb_pelanggan`
+--
+
+INSERT INTO `tb_pelanggan` (`id_pelanggan`, `nama_pelanggan`, `no_internet`, `no_voice`, `odp`, `port`, `sn_ont`, `tipe`, `email`, `no_hp`, `alamat`) VALUES
+(1, 'Nuril Muslichin', '142265342612', '02851627162', 'ODP-PKL-FN/107', 2, 'FHTT16T26H77', 'Indihome', 'nurilmuslichin16@gmail.com', '085229531170', 'Jl Pemuda No.45'),
+(2, 'Seto Prayoga Putra', '14226537263', '0285172612', 'ODP-KDW-FA/087', 3, 'ZTEG09JUYD66', 'BGES / VPN IP', 'seto@gmail.com', '085261526252', 'Jl Panjang No 22 Gg 1'),
+(3, 'Fitriyah', '14222737648', '0285779228', 'ODP-KDW-FA/001', 8, 'FHTT87263527', 'WIFI ID', 'fitriyah@gmail.com', '087663773642', 'Kedungwuni Gg13 ');
+
 -- --------------------------------------------------------
 
 --
@@ -92,6 +107,14 @@ CREATE TABLE `tb_teknisi` (
   `mitra` varchar(50) NOT NULL,
   `status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_teknisi`
+--
+
+INSERT INTO `tb_teknisi` (`id_telegram`, `nik`, `nama_teknisi`, `mitra`, `status`) VALUES
+(232335771, 997282, 'Seto Setiawan', 'TELKOM AKSES', 0),
+(232335772, 999916, 'Nur', 'KOPEGTEL', 1);
 
 -- --------------------------------------------------------
 
@@ -108,6 +131,13 @@ CREATE TABLE `tb_user` (
   `status` tinyint(1) NOT NULL,
   `last_login` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_user`
+--
+
+INSERT INTO `tb_user` (`id_user`, `nama_lengkap`, `email`, `password`, `level`, `status`, `last_login`) VALUES
+(1, 'Team Leader', 'admin@gmail.com', '$2y$10$IjRv6I5uuuxyc0VViNufa.1k8KOoEIbuYrO8onFVmmvgJ2nE4ZH3G', 1, 1, '2023-01-02 23:44:48');
 
 --
 -- Indexes for dumped tables
@@ -151,7 +181,7 @@ ALTER TABLE `tb_user`
 -- AUTO_INCREMENT untuk tabel `tb_gangguan`
 --
 ALTER TABLE `tb_gangguan`
-  MODIFY `id_gangguan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_gangguan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_log`
@@ -163,19 +193,19 @@ ALTER TABLE `tb_log`
 -- AUTO_INCREMENT untuk tabel `tb_pelanggan`
 --
 ALTER TABLE `tb_pelanggan`
-  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_teknisi`
 --
 ALTER TABLE `tb_teknisi`
-  MODIFY `id_telegram` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_telegram` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=232335773;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
